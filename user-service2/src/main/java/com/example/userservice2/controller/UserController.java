789c45d777bd446fdf234798024ee9f3ad569f8a
seongjin-an <seongjin4us@gmail.com>
2023-05-15 23:22:17 +0900

    User Microservice - welcome

---
+++ user-service2/src/main/java/com/example/userservice2/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.userservice2.controller;
+
+import com.example.userservice2.vo.Greeting;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private Environment env;
+
+    private Greeting greeting;
+
+    public UserController(Environment env, Greeting greeting) {
+        this.env = env;
+        this.greeting = greeting;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+}
