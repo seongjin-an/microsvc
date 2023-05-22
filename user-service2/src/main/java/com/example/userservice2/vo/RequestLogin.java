fe652ca31b93092645dfefed6e9ffb3936018aa2
seongjin-an <seongjin4us@gmail.com>
2023-05-22 23:52:34 +0900

    Users Microservice - AuthenticationFilter 추가

---
+++ user-service2/src/main/java/com/example/userservice2/vo/RequestLogin.java
@@ -0,0 +1,16 @@
+package com.example.userservice2.vo;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RequestLogin {
+    @NotNull(message = "Email cannot be null")
+    @Size(min = 2, message = "Email not be less than two characters")
+    private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password must be equals or greater than 8 characters")
+    private String password;
+}
